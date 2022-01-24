73b66e576db476f006a5bb91f1f5323462eca585
Ed Vidal <develone@sbcglobal.net>
2022-01-24 05:36:39 -0700

    This now uses the liblifting.a & libcrc.a which are created from 3 C files lifting.c & (crc.c head-tail.c)

--- pico-lifting/main.c
+++ pico-lifting/main.c
@@ -8,6 +8,7 @@
 #include "pico/stdlib.h"
 #include "lifting.h"
 #include "crc.h"
+#include "head-tail.h"
 
 #define DBUG 0
 #define DBUG1 0
@@ -48,62 +49,6 @@ const short int a[];
 unsigned char CRCTable[256];
  
 
-char * bump_head(char * head, char * endofbuf,char * topofbuf) {
- 
-	if(head == endofbuf) {
-
-		
-			//printf("head == endofbuf\n");
-			head = topofbuf;
-	}
-	else {
-		//printf("head < endofbuf\n");
-		head = head + 1;
-	}
- 
-	
-	return((char *)head);
-}
-char * bump_tail(char * tail,char * endofbuf,char * topofbuf) {
-	
-	if(tail == endofbuf) {
-
-		
-			//printf("tail == endofbuf\n");
-			tail = topofbuf;
-	}
-	else {
-		//printf("tail < endofbuf\n");
-		tail = tail + 1;
-	}
- 
-	
-	return((char *)tail);
-}
-char * dec_head(char * head,char * endofbuf,char * topofbuf) {
-	if(head == topofbuf) {
-			//printf("head == topofbuf\n");
-			//head = topofbuf;
-	}
-	else {
-		//printf("head < topofbuf\n");
-		head = head - 1;
-	}
-
-	return((char *)head);
-}
-char * dec_tail(char * tail,char * endofbuf,char * topofbuf) {
-	if(tail == topofbuf) {
-			printf("tail == topofbuf\n");
-			//head = topofbuf;
-	}
-	else {
-		printf("tail < topofbuf\n");
-		tail = tail - 1;
-	}
-
-	return((char *)tail); 
-}
 int read_tt(char * head, char * endofbuf,char * topofbuf) {
 
 	int i,numtoread = 64;
