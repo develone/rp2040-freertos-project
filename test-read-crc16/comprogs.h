c8194678642985f8180e58f659513478f5e85ad4
Ed Vidal Jr <develone@sbcglobal.net>
2022-06-15 15:34:31 -0600

    klt now using data from l64.pgm Pico Shell - Copyright (C) 1883 Thomas Edison This program comes with ABSOLUTELY NO WARRANTY. This is free software, and you are welcome to redistribute it under certain conditions. See LICENSE file for details.
    console on UART (detected 24 rows, 80 columns)
    
    enter command, hit return for help
    
    32 /: mount
    6 mount mount filesystem
    mounted
    /: lsklt l64.pgm r.pgm 0
    4 l64.pgm r.pgm 0
    P5
    255
    161 159 157 155 160 170 168 134 97 106 109 109 109 117 124 129 132 133 132 133
    opening a file to write the results
    need to copy the data received from host to img1
    img1 = 0x20007c08 img2 = 0x20008c08
    0 img1 161 ptrs.buf 161
    1 img1 159 ptrs.buf 159
    2 img1 157 ptrs.buf 157
    3 img1 155 ptrs.buf 155
    4 img1 160 ptrs.buf 160
    4091 img1 123 ptrs.buf 123
    4092 img1 94 ptrs.buf 94
    4093 img1 58 ptrs.buf 58
    4094 img1 61 ptrs.buf 61
    4095 img1 91 ptrs.buf 91
    need to copy the data from img1 to img2
    0 img2 161 img1 161
    1 img2 159 img1 159
    2 img2 157 img1 157
    3 img2 155 img1 155
    4 img2 160 img1 160
    4091 img2 123 img1 123
    4092 img2 94 img1 94
    4093 img2 58 img1 58
    4094 img2 61 img1 61
    4095 img2 91 img1 91
    Feature #0:  (30.000000,28.000000) with value of 13516
    Feature #1:  (33.000000,38.000000) with value of 5855

---
+++ test-read-crc16/comprogs.h
@@ -0,0 +1,47 @@
+static void echo_key(char c);
+static void parse_cmd(void);
+char* full_path(const char* name);
+static void xmodem_cb(uint8_t* buf, uint32_t len);
+static bool check_mount(bool need);
+static bool check_name(void);
+static void put_cmd(void);
+int check_cp_parms(char** from, char** to, int copy);
+static void mv_cmd(void);
+static void cp_cmd(void);
+static void get_cmd(void);
+
+#define imgsize 4096
+//#define imgsize 512
+struct PTRs
+{
+  /*This is the buffer for inp & output
+     2048 x 2048 = 4194304
+     256 x 256 = 65536
+     64 x 64 = 4096
+   */
+  short int inpbuf[imgsize * 2];
+  short int *inp_buf;
+  short int *out_buf;
+  short int flag;
+  short int w;
+  short int h;
+  short int *fwd_inv;
+  short int fwd;
+  short int *red;
+  char *head;
+  char *tail;
+  char *topofbuf;
+  char *endofbuf;
+} ptrs;
+
+unsigned char *img1, *img2;
+int ncols, nrows, i;
+unsigned char tt[128];
+const char src[] = "Hello, world! ";
+const short int a[];
+int nFeatures = 100;
+KLT_TrackingContext tc;
+KLT_FeatureList fl;
+//const unsigned char CRC7_POLY = 0x91;
+unsigned char CRCTable[256];
+
