3672c3ae6e3b3a0ca1cb5bced97954e256a79d28
Ed Vidal Jr <develone@sbcglobal.net>
2024-10-23 12:45:59 -0600

    upgrade pico2 which requires my-2.0.0 of pico-sdk

--- compile.sh
+++ compile.sh
@@ -2,6 +2,6 @@
 rm -rf build 
 mkdir build
 cd build/
-cmake -DPICO_BOARD=pico -DFREERTOS_KERNEL_PATH:PATH=../freertos/FreeRTOS-Kernel/ ..
+cmake -DPICO_BOARD=pico2 -DFREERTOS_KERNEL_PATH:PATH=../freertos/FreeRTOS-Kernel/ ..
 make
 
