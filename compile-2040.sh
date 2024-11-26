7349e7abbecc9605e20ee1976e651c0bc37bb7c6
Edward Vidal Jr <develone@sbcglobal.net>
2024-11-26 05:08:32 -0700

    rm build-2040 and build-2350

--- compile-2040.sh
+++ compile-2040.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-rm -rf build 
+rm -rf build-2040 
 mkdir build-2040
 cd build-2040
 cmake -DPICO_BOARD=pico ..
--- compile-2350.sh
+++ compile-2350.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-rm -rf build 
+rm -rf build-2350 
 mkdir build-2350
 cd build-2350
 cmake -DPICO_BOARD=pico2 ..
